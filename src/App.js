15c62315d8d48d6fff030fa474bd28868872e34a
Bryan <bmontalvan100@gmail.com>
2023-03-19 20:36:39 -0400

    Initial commit for landing-page
    This commit contains the following:
    - Components on landing page
    - React-router installation and implementations (installed react-router)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Explore from './pages/Explore';
 import HomePage from './pages/Home';
+
+
 function App() {
     return (
         <ChakraProvider>
-            <div className="App">
-                <HomePage />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/explore" element={<Explore />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </ChakraProvider>
     );
 }
