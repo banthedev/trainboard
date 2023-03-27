48ec8bd179231f5680c6d4118aac9aee0c0bcb9c
Bryan Mointalvan <bryan_montalvanarevalo@student.uml.edu>
2023-03-27 14:09:59 -0400

    Improved sign-in/out UI
    This commit does the following changes:
    - Adds background to /signup and /signin
    - Changed text colors in these pages to contrast the black background
    - Added basic auth error handling
    - Added a "Go back home" button
    
    Misc:
    - Removes background image with a watermark

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,102 +9,112 @@ import {
     Button,
     Heading,
     Text,
+    Highlight,
 } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 
 export default function SignUp() {
-   // Error state
-   const [error, setError] = useState('');
-   // Email & Pass state
-   const [email, setEmail] = useState('');
-   const [password, setPassword] = useState('');
-   const [confirmationPassword, setConfirmationPassword] = useState('');
-   // Loading State, disabling button when calling the firebase hook
-   const [loading, setLoading] = useState(false);
+    // Error state
+    const [error, setError] = useState('');
+    // Email & Pass state
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmationPassword, setConfirmationPassword] = useState('');
+    // Loading State, disabling button when calling the firebase hook
+    const [loading, setLoading] = useState(false);
 
-   const navigate = useNavigate();
-   // Create user hook
-   const { createUser } = UserAuth();
+    const navigate = useNavigate();
+    // Create user hook
+    const { createUser } = UserAuth();
 
-   async function handleSubmit(e) {
-       // Prevent Page Reload
-       e.preventDefault();
+    async function handleSubmit(e) {
+        // Prevent Page Reload
+        e.preventDefault();
 
-       // Make sure password and confirmation password are the same
-       if (password !== confirmationPassword) {
-           return setError("Passwords do not match :(");
-       }
+        // Make sure password and confirmation password are the same
+        if (password !== confirmationPassword) {
+            return setError("Error: passwords do not match");
+        }
 
-       try {
-           setError('');
-           setLoading(true);
-           await createUser(email, password);
-           navigate('/dashboard')
-       } catch (e) {
-           setError("Failed to create an account");
-           console.log(e.message);
-       }
-       setLoading(false);
-   };
+        try {
+            setError('');
+            setLoading(true);
+            await createUser(email, password);
+            navigate('/dashboard')
+        } catch (e) {
+            setError("Error: failed to create an account");
+            console.log(e.message);
+        }
+        setLoading(false);
+    };
     return (
         <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
->
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-            <Stack align={'center'}>
-                <Image src="trainboard_full.png" w="300px" />
-                <Heading fontSize={'4xl'}>Sign up for TrainBoard</Heading>
-                <Text fontSize={'lg'} color={'gray.600'}>
-                    Workouts That Work for You
-                </Text>
-                {error &&
-                        <Text
-                            className='text-lg text-rose-500 text-center'
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            backgroundImage={
+                'red_background.jpg'
+            }
+            backgroundSize={'cover'}
+            backgroundPosition={'center center'}
+        >
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Image src="trainboard_full.png" w="300px" />
+                    <Heading fontSize={'4xl'} color="white">Sign up for TrainBoard</Heading>
+                    <Text fontSize={'lg'} color={'red.400'}>
+                        Workouts That Work for You
+                    </Text>
+                    {error &&
+                        <Highlight
+                            query={error}
+                            styles={{ px: '2', py: '1', rounded: 'full', bg: 'red.300' }}
                         >
                             {error}
-                        </Text>
+                        </Highlight>
                     }
-            </Stack>
-            <form onSubmit={handleSubmit}
-                p={8}>
-                <Stack spacing={4}>
-                    <FormControl id="email" onChange={(e) => setEmail(e.target.value)}>
-                        <FormLabel>Email address</FormLabel>
-                        <Input type="email" />
-                    </FormControl>
-                    <FormControl id="password" onChange={(e) => setPassword(e.target.value)}>
-                        <FormLabel>Password</FormLabel>
-                        <Input type="password" />
-                    </FormControl>
-                    <FormControl id="passwordConfirm" onChange={(e) => setConfirmationPassword(e.target.value)}>
-                        <FormLabel>Password Confirmation</FormLabel>
-                        <Input type="password" />
-                    </FormControl>
-                    <Stack spacing={10}>
-                        <Stack
-                            direction={{ base: 'column', sm: 'row' }}
-                            align={'center'}
-                            justify={'space-between'}>
-                            <Link color={'blue.400'} to='/signin'>Already have an account? Sign in</Link>
-                        </Stack>
-                        <Button
-                            type="submit"
-                            bg={'red.500'}
-                            color={'white'}
-                            disabled={loading}
-                            _hover={{
-                                bg: 'red.600',
-                            }}>
-                            Sign Up
-                        </Button>
-                    </Stack>
                 </Stack>
-            </form>
-        </Stack>
-    </Flex>    
+                <form onSubmit={handleSubmit}
+                    p={8}>
+                    <Stack spacing={4} color="white">
+                        <FormControl id="email" onChange={(e) => setEmail(e.target.value)} isRequired>
+                            <FormLabel>Email address</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+                        <FormControl id="password" onChange={(e) => setPassword(e.target.value)} isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
+                        <FormControl id="passwordConfirm" onChange={(e) => setConfirmationPassword(e.target.value)} isRequired>
+                            <FormLabel>Password Confirmation</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
+                        <Stack spacing={8}>
+                            <Stack
+                                direction={{ base: 'column', sm: 'row' }}
+                                align={'center'}
+                                justify={'space-between'}>
+                                <Text>Already have an account? <Link style={{ color: "#E53E3E" }} to="/signin">Sign in</Link></Text>
+                            </Stack>
+                            <Button
+                                type="submit"
+                                bg={'red.500'}
+                                color={'white'}
+                                disabled={loading}
+                                _hover={{
+                                    bg: 'red.600',
+                                }}>
+                                Sign Up
+                            </Button>
+                            <Link to="/">
+                                <Text color="red.300">Go back home</Text>
+                            </Link>
+                        </Stack>
+                    </Stack>
+                </form>
+            </Stack>
+        </Flex>
     )
 }
 
