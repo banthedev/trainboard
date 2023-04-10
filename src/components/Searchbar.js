52fa6d2c3f2d44e9291261959dd7d13030b560a6
Anderson Nouv <anderson.nouv@gmail.com>
2023-04-10 17:33:12 -0400

    Updated Searchbar background
    Got the background of search bar to stay white. Simply just changed the variant from "filled" to "outline". Also changed "backgroundColor" to "bg", not sure if it makes a difference but followed someone's example just to be safe

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,8 +2,8 @@ import { Input } from '@chakra-ui/react'
 
 export default function Searchbar(){
     return(
-        <Input variant="filled" placeholder='Search Here' size='lg' width="50%" 
+        <Input variant="outline" placeholder='Search Here' size='lg' width="50%" 
         float="left" marginLeft='5%' marginTop="1%" focusBorderColor='black'
-        _placeholder={{ color: 'black' }} backgroundColor='white' />
+        _placeholder={{ color: 'black' }} bg='white' />
     );
 }
