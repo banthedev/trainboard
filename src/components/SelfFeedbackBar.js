866cc8e7ffc428d5fcbe251ef7e082dfad945290
KyleGaudet <113302539+KyleGaudet@users.noreply.github.com>
2023-04-09 21:50:57 -0400

    start of user workout deletion (#40)
    -added self feedback bar variation to use when users view their own workouts
    
    - began implementation of making the self feedback bar display

---
+++ src/components/SelfFeedbackBar.js
@@ -0,0 +1,50 @@
+import {
+    Box,
+    Flex,
+    HStack,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function SelfFeedbackBar() {
+    return (
+        <>
+            <Box bg="white" px={4} alignItems="center" width="fit-content" margin="auto" border="solid 5px black" marginTop="1%">
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <HStack margin="auto" display={{ base: 'none', md: 'flex' }}>
+                        <Button bg="red">
+                            Delete Workout
+                        </Button>
+
+                        <Flex alignItems={'center'}>
+                            <Menu >
+                                <MenuButton
+                                    as={Button}
+                                    cursor={'pointer'}
+                                    label={'Muscle Group...'}
+                                    bg="gold"
+                                    >
+                                    Export
+                                </MenuButton>
+                                <MenuList>
+                                    <MenuItem>Image</MenuItem>
+                                    <MenuDivider />
+                                    <MenuItem>Pdf</MenuItem>
+                                </MenuList>
+                            </Menu>
+                        </Flex>
+
+                        <Button bg="green" color="white">
+                            Favorite
+                        </Button>
+                    </HStack>
+                </Flex>
+            </Box>
+        </>
+    );
+}
