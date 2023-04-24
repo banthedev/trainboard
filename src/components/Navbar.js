ec785483a2801b304e0e6af7dd5e00bcaaf8c34c
Bryan Mointalvan <bryan_montalvanarevalo@student.uml.edu>
2023-04-24 01:13:27 -0400

    Performance and misc
    This commit contains the following:
    - Improved queries and the overall workflow for how the dashboard renders the workouts (used caching methods built-in to react)
    - Added static "/" route to the navbar icon not showing up in dynamic paths
    - Added Kangaroo as the default profile picture as agreed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -90,7 +90,7 @@ export default function Navbar() {
                     <HStack spacing={8} alignItems={'center'}>
                         <Box w="50px">
                             <Image
-                                src="trainboard_icon.png"
+                                src="/trainboard_icon.png"
                                 alt="TrainBoard"
                             />
                         </Box>
@@ -141,7 +141,7 @@ export default function Navbar() {
                                     <Avatar
                                         size={'sm'}
                                         src={
-                                            'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                                            '/kangaroo.png'
                                         }
                                     />
                                 </MenuButton>
